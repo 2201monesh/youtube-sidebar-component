b41d3e94a199d4e7d10ee07434101d779df4b2bf
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-09-17 23:45:10 +0530

    Styling upper-card-component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
 import { useEffect, useState } from "react";
+import Sidebar from "./components/Sidebar";
 
 function App() {
 
   const [videos, setVideos] = useState([]);
 
-  // const getData = () => {
-  //   fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=30&playlistId=UU0rE2qq81of4fojo-KhO5rg&key=AIzaSyCO1PIrUzbrYeqirBdeMxVB56pNErUtWXY')
-  //   .then(res => {
-  //     return res.json()
-  //   })
-  //   .then(data => {
-  //     data.items.forEach((curr) => {
-  //       // vidTitle = curr.snippet.title;
-  //       console.log(curr.snippet.title);
-  //     })
-  //   })
-  // }
-
   const getData = async () => {
-    const apiCall = await fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=30&playlistId=UU0rE2qq81of4fojo-KhO5rg&key=AIzaSyCO1PIrUzbrYeqirBdeMxVB56pNErUtWXY');
+    const apiCall = await fetch(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=30&playlistId=UU0rE2qq81of4fojo-KhO5rg&key=AIzaSyCO1PIrUzbrYeqirBdeMxVB56pNErUtWXY`);
 
     const data = await apiCall.json();
     console.log(data.items);
     setVideos(data.items);
   }
 
-  useEffect(() => {
-    getData();
-  }, []);
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
   return (
-    <div className="App">
-      {videos.map((curr) => <a href={`https://www.youtube.com/watch?v=${curr.snippet.resourceId.videoId}`}>{curr.snippet.resourceId.videoId}</a>)}
-      {videos.map((curr) => <img src={curr.snippet.thumbnails.default.url} />)}
+    <div className="app">
+      {/* {videos.map((curr) => <a href={`https://www.youtube.com/watch?v=${curr.snippet.resourceId.videoId}`}>{curr.snippet.resourceId.videoId}</a>)}
+      {videos.map((curr) => <img src={curr.snippet.thumbnails.default.url} />)} */}
+
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='sidebar-main'>
+      <div className="sidebar-upper">
+        <div className="sidebar-upper-left">Left</div>
+        <div className="sidebar-upper-right">
+            <p className='logo-1'>logo1</p>
+            <p className='logo-2'>logo2</p>
+        </div>
+      </div>
+
+
+
+
+      <div className="sidebar-lower">Sidebar Lower</div>
+    </div>
+  )
+}
+
+export default Sidebar;
