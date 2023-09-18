50881929ef23a6d00e1c46cae578defd15b271ea
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-09-18 19:51:20 +0530

    Color UI

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import { RxCross1 } from 'react-icons/rx';
+import { LuMoreVertical } from 'react-icons/lu';
+import { TiArrowShuffle } from 'react-icons/ti';
+import { TbArrowsRightLeft } from 'react-icons/tb';
 
 function Sidebar() {
   return (
     <div className='sidebar-main'>
       <div className="sidebar-upper">
-        <div className="sidebar-upper-left">Left</div>
+        <div className="sidebar-upper-left">
+            <div className="sidebar-upper-left-1">Main Heading goes here</div>
+            <div className="sidebar-upper-left-2">Sub heading goes here</div>
+            <div className="sidebar-upper-left-3">
+                <p className='logo logo-left'><TbArrowsRightLeft /></p>
+                <p className='logo logo-left' ><TiArrowShuffle /></p>
+            </div>
+        </div>
+
         <div className="sidebar-upper-right">
-            <p className='logo-1'>logo1</p>
-            <p className='logo-2'>logo2</p>
+            <p className='logo-1'><RxCross1 className='logo' /></p>
+            <p className='logo-2'><LuMoreVertical className='logo' /></p>
         </div>
       </div>
 
